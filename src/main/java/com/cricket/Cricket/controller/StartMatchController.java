9de4825283128fab34e5822d2408d8d3f7153f9d
Tarun PSV <tarunpsv@gmail.com>
2023-03-15 18:26:15 +0530

    Created StartMatchController

---
+++ src/main/java/com/cricket/Cricket/controller/StartMatchController.java
@@ -0,0 +1,18 @@
+package com.cricket.Cricket.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class StartMatchController {
+    @Autowired
+    private StartMatchService startMatchService;
+    @RequestMapping(method = RequestMethod.POST,value = "/start")
+    public String start(@RequestBody GameDataDTO gameDataDTO){
+        return startMatchService.start(gameDataDTO);
+    }
+}
